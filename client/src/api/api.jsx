f2ae4c62200d16039edb59ea12c73cf4b398bcaf
co-den <iamagugbueikenna@gmail.com>
2025-08-20 05:09:50 +0100

    first commit

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API = import.meta.env.VITE_API_BASE || "https://flex-1-o88e.onrender.com";
 
 
 const pluck = (p) => p.then((r) => r.data.result);
